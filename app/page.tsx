9578a407b3f6324b5ea2304f38d6d3d7ca500041
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-06-09 18:14:43 +0330

    test Git

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center flex-col">
       <h1 className="text-4xl font-bold">Davvat app</h1>
+      <p className="text-lg mt-2">sothesom</p>
     </div>
   );
 }
